0604afea375917655a7a50cc3c5a001d2a186b65
Rakesh verma <rakesh.verma0487@gmail.com>
2014-04-07 11:08:59 +0530

    struct usages

---
+++ ruby_struct_use.rb
@@ -0,0 +1,14 @@
+# A Struct is a convenient way to bundle a number of attributes together, using accessor methods, without having to write an explicit class.
+
+Struct.new("Space", :x, :y, :z)  #=> Struct::Space
+
+origin = Struct::Space.new(0, 0, 0)
+
+# practical usages
+
+class Space < Struct.new(:x, :y, :z); end
+origin = Space.new(0, 0, 0) 
+
+Space = Struct.new(:x, :y, :z)
+origin = Space(0, 0, 0)
+
